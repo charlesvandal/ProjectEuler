51200ced813ac99e58a5e4b3ca7f5a336c34e3a0
Charles-David Lachance <charles-david.lachance@vooban.com>
2024-10-30 11:09:20 -0400

    First draft of ratatui

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,25 @@ mod solutions;
 mod utilities;
 mod parser;
 mod handler;
+mod tui;
+mod metadata_parser;
 
-use crate::parser::arguments_parser::ArgumentsParser;
-use handler::{arguments_handler::handle_arguments, inputs_handler::handle_inputs};
+// fn main() {
+//     let mut arguments_parser = ArgumentsParser::new();
+//     let mut do_exit = false;
+//
+//     handle_arguments(&mut arguments_parser);
+//
+//     while !do_exit {
+//         do_exit = handle_inputs(&mut arguments_parser);
+//     }
+// }
 
-fn main() {
-    let mut arguments_parser = ArgumentsParser::new();
-    let mut do_exit = false;
+fn main() -> std::io::Result<()> {
+    let problems = metadata_parser::read_problems_from_file("./src/metadata.json").unwrap();
 
-    handle_arguments(&mut arguments_parser);
-
-    while !do_exit {
-        do_exit = handle_inputs(&mut arguments_parser);
-    }
+    let mut terminal = ratatui::init();
+    let app_result = tui::App::default().run(&mut terminal, problems);
+    ratatui::restore();
+    app_result
 }
---
+++ src/tui.rs
@@ -0,0 +1,129 @@
+use crate::metadata_parser::Problem;
+use crate::runner::solution_runner;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::widgets::{List, ListItem, ListState, StatefulWidget};
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Rect},
+    style::Stylize,
+    symbols::border,
+    text::Line,
+    widgets::{
+        block::{Position, Title},
+        Block,
+    },
+    DefaultTerminal, Frame,
+};
+use std::io;
+
+#[derive(Debug, Default)]
+pub struct App {
+    state: ListState,
+    problems: Vec<Problem>,
+    exit: bool,
+}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal, problems: Vec<Problem>) -> io::Result<()> {
+        self.problems = problems;
+        self.state = ListState::default();
+        self.state.select(Some(0));
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_stateful_widget(self, frame.area(), &mut ());
+    }
+
+    // TODO Could add mouse event?
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Esc | KeyCode::Char('q') => self.exit(),
+            KeyCode::Up | KeyCode::Char('w') => self.select_prev(),
+            KeyCode::Down | KeyCode::Char('s') => self.select_next(),
+            KeyCode::Enter | KeyCode::Char('e') => self.run_solution(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn select_next(&mut self) {
+        if (self.state.selected().unwrap() < self.problems.len() - 1) {
+            self.state.select_next();
+        }
+    }
+
+    fn select_prev(&mut self) {
+        self.state.select_previous();
+    }
+
+    // TODO This break the tui since it writes directly to the standard output
+    /*
+        Will need to update how the state is handled.
+        In the implementation of Stateful Widget, I need a custom AppState instead of keeping a ref in the App.
+        The AppState will keep a reference of the ListState and also have a something like a DisplayState to know
+        what solution was run and what to display.
+     */
+    fn run_solution(&mut self) {
+        let solution_index = self.state.selected().unwrap() as i8;
+        let solution_id = solution_runner::SolutionsID::from(solution_index + 1);
+        solution_runner::run_solution(&solution_id);
+    }
+}
+
+impl StatefulWidget for &App {
+    type State = ();
+
+    fn render(self, area: Rect, buf: &mut Buffer, state: &mut Self::State) {
+        let title = Title::from(" Project Euler ".bold());
+        let instructions = Title::from(Line::from(vec![
+            " Select ".into(),
+            "<Enter>, <e>".blue().bold(),
+            " Previous ".into(),
+            "<Up>, <w>".blue().bold(),
+            " Next ".into(),
+            "<Down>, <s>".blue().bold(),
+            " Quit ".into(),
+            " <Esc>, <q> ".blue().bold(),
+        ]));
+
+        let left_block = Block::bordered()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .border_set(border::THICK);
+
+
+        let items: Vec<ListItem> = self.problems
+            .iter()
+            // Probably a better way to format the line
+            .map(|problem| ListItem::new(format!("{:<4}  {}", problem.id, problem.name)))
+            .collect();
+
+        let list = List::new(items)
+            .block(left_block)
+            .highlight_style(ratatui::style::Style::default().fg(ratatui::style::Color::LightYellow))
+            .highlight_symbol(">> ")
+            .render(area, buf, &mut self.state.clone());
+    }
+}
